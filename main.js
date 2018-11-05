6ea409adb0d8274f136fdc10c082f4b02e4f41af
DimitrisKapoulas <kapoulasd@gmail.com>
2018-11-05 16:37:32 +0200

    initialize

---
+++ main.js
@@ -0,0 +1,6 @@
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+var userAge = prompt("What is your age?");
+
+console.log ("Users full name "+firstName+" "+lastName);
+console.log("users age "+userAge);
